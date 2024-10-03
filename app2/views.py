14b1a50d97c8e655dedfe74a0abe2e53d69713e3
alina <aantonova786@gmail.com>
2024-10-03 14:13:58 +0500

    #11 Изменил вывод в функции представления view1(в приложении app2) * отредактировала приложения

--- app2/views.py
+++ app2/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
+def view1(request):
+    return HttpResponse("This is view 1 in app2")
+
 def view2(request):
-    return HttpResponse('View 2 from app2')
+    return HttpResponse("This is view 2 in app2")
+#   return HttpResponse("В жизни главное - это велосипед")
+
+
+def view3(request):
+    return HttpResponse("This is view 3 in app2")
